75d44b18a96e6caa345d8e70bf5ea77f3a834b4d
theun1c <belovdima1122@yandex.ru>
2026-05-30 02:08:38 +0300

    feat: create service component

---
+++ Variant1/LearnSchoolAvalonia/LearnSchoolAvalonia/Views/Components/ServiceCard.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LearnSchoolAvalonia.Components;
+
+public partial class ServiceCard : UserControl
+{
+    public ServiceCard()
+    {
+        InitializeComponent();
+    }
+}
